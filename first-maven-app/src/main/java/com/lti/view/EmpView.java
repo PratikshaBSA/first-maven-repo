66af60da0d85a62a2639bfa2779c417c243305b3
PratikshaBSA <acharya.p1998@gmail.com>
2021-03-25 10:01:06 +0530

    First Commit

---
+++ first-maven-app/src/main/java/com/lti/view/EmpView.java
@@ -0,0 +1,14 @@
+package com.lti.view;
+
+import com.lti.model.Employee;
+
+public class EmpView {
+	public static void main(String [] args){
+		Employee emp=new Employee();
+		emp.setEmpId(1001);
+		emp.setEmpName("John");
+		System.out.println("ID :" +emp.getEmpId());
+		System.out.println("NAME :" +emp.getEmpName());
+	}
+
+}
